4f21fb3597dc45e4f1625c6a2c6f936b97f30377
Amy Yang <jy152@duke.edu>
2016-11-13 14:29:37 -0500

    heroku mongo addon

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 export default callback => {
 	// connect to a database if needed, the pass it to `callback`:
   // Configure database
-  mongoose.connect('mongodb://localhost:27017/hackduke-hardware');
+  mongoose.connect('mongodb://heroku_vkhkvzv3:pm94t0nee6vt444c2b7pm195rn@ds151927.mlab.com:51927/heroku_vkhkvzv');
   mongoose.connection.on('error',function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);

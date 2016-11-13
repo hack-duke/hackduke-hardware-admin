d58a96054fa3631360b03acb009aae847d2cbd7c
Amy Yang <jy152@duke.edu>
2016-11-13 14:38:36 -0500

    heroku mongodb

--- src/db.js
+++ src/db.js
@@ -2,9 +2,10 @@ import mongoose from 'mongoose';
 export default callback => {
 	// connect to a database if needed, the pass it to `callback`:
   // Configure database
-  mongoose.connect('mongodb://heroku_vkhkvzv3:pm94t0nee6vt444c2b7pm195rn@ds151927.mlab.com:51927/heroku_vkhkvzv');
-  mongoose.connection.on('error',function() {
+  mongoose.connect('mongodb://hackduke:dendenHacker@ds151927.mlab.com:51927/heroku_vkhkvzv3');
+  mongoose.connection.on('error',function(err) {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  console.log(err);
   process.exit(1);
 });
 	callback(mongoose);

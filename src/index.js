c5bb433f2c0b40001dce0c46e1827645a5cbf237
Amy Yang <jy152@duke.edu>
2016-11-13 18:27:05 -0500

    diable auth

--- src/index.js
+++ src/index.js
@@ -20,26 +20,26 @@ app.use(cors({
 	exposedHeaders: config.corsHeaders
 }));
 
-var auth = function (req, res, next) {
-  function unauthorized(res) {
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    return res.sendStatus(401);
-  };
-
-  var user = basicAuth(req);
-
-  if (!user || !user.name || !user.pass) {
-    return unauthorized(res);
-  };
-
-  if (user.name === 'foo' && user.pass === 'bar') {
-    return next();
-  } else {
-    return unauthorized(res);
-  };
-};
-
-app.use('/', auth, routes);
+// var auth = function (req, res, next) {
+//   function unauthorized(res) {
+//     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+//     return res.sendStatus(401);
+//   };
+//
+//   var user = basicAuth(req);
+//
+//   if (!user || !user.name || !user.pass) {
+//     return unauthorized(res);
+//   };
+//
+//   if (user.name === 'foo' && user.pass === 'bar') {
+//     return next();
+//   } else {
+//     return unauthorized(res);
+//   };
+// };
+//
+// app.use('/', auth, routes);
 
 app.use(bodyParser.json({
 	limit : config.bodyLimit
